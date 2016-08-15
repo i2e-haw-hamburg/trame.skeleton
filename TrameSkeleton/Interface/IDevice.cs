dcdc65b5a7f5bbb1f7de1043321264e214c43651
Christian Blank <christian.blank@haw-hamburg.de>
2016-08-15 15:05:05 +0200

    Add device

---
+++ TrameSkeleton/Interface/IDevice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Trame.Interface
+{
+    /// <summary>
+    /// I device.
+    /// </summary>
+    interface IDevice
+    {
+        /// <summary>
+        /// Gets the skeleton.
+        /// </summary>
+        /// <returns>The skeleton.</returns>
+        ISkeleton GetSkeleton();
+
+        /// <summary>
+        /// Gets the skeleton.
+        /// </summary>
+        /// <returns>The skeleton.</returns>
+        /// <param name="baseSkeleton">Base skeleton.</param>
+        ISkeleton GetSkeleton(ISkeleton baseSkeleton);
+
+        /// <summary>
+        /// Stop this instance.
+        /// </summary>
+        void Stop();
+
+        void Start();
+        /// <summary>
+        /// Occurs when new skeleton.
+        /// </summary>
+        event Action<ISkeleton> NewSkeleton;
+    }
+}
